39698370515a3a7708ec8b822acd89457dbd39b8
Magyar-Hunor Tamas <hunor@makers.tech>
2022-05-24 23:14:56 +0100

    Add models/index

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const dbConfig = require('../config/db.config.js');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db = {};
+db.sequelize = Sequelize;
+db.sequelize = sequelize;
+db.restaurants = require('./restaurant.model.js')(sequelize, Sequelize);
+module.exports = db;
