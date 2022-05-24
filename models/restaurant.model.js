3736af556ca0f6b8f5abee89a70a1f7743513d37
Magyar-Hunor Tamas <hunor@makers.tech>
2022-05-24 23:24:36 +0100

    Add Restaurant model

---
+++ models/restaurant.model.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, Sequelize) => {
+    const Restaurant = sequelize.define('restaurant', {
+        name: {
+            type: Sequelize.STRING
+        },
+        type: {
+            type: Sequelize.STRING
+        },
+        description: {
+            type: Sequelize.STRING
+        },
+        address: {
+            type: Sequelize.STRING
+        },
+        phone: {
+            type: Sequelize.STRING
+        },
+        owner: {
+            type: Sequelize.STRING
+        },
+        opendate: {
+            type: Sequelize.DATE
+        },
+        open: {
+            type: Sequelize.BOOLEAN
+        }
+    },
+    {
+        timestamps: true
+    })
+}
