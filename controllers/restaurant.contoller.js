de78932711f1fa4526db9f0a8cd746738b2c5500
Magyar-Hunor Tamas <hunor@makers.tech>
2022-05-25 21:07:10 +0100

    Add Restaurant controller

---
+++ controllers/restaurant.contoller.js
@@ -0,0 +1,33 @@
+const db = require('../models');
+const Restaurant = db.restaurants;
+const Op = db.sequelize.Op;
+
+// Create and Save a new Restaurant
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Restaurants from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Restaurant with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Restaurant by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Restaurant with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Find all open Restaurants
+exports.findAllOpen = (req, res) => {
+
+};
