4698279340482dafaf36935dc8e994a48e01cd49
Magyar-Hunor Tamas <hunor@makers.tech>
2022-05-25 21:13:10 +0100

    Add postgres docker container

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "123",
-    DB: "testdb",
+    PASSWORD: "postgres",
+    DB: "mern_postgres",
     dialect: "postgres",
     pool: {
         max: 5,
---
+++ db/init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE mern_postgres;
