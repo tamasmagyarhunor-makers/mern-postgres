89034c24be5da05261a1e3501a815a1aa4981572
Magyar-Hunor Tamas <hunor@makers.tech>
2022-05-28 23:40:59 +0100

    Update models/index.js to know about MenuItems and its relation between Menu

--- models/index.js
+++ models/index.js
@@ -17,6 +17,7 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.restaurants = require('./restaurant.model.js')(sequelize, Sequelize);
 db.menus = require('./menu.model.js')(sequelize, Sequelize);
+db.menuitems = require('./menuitem.model.js')(sequelize, Sequelize);
 
 db.restaurants.hasMany(db.menus, {
     foreignKey: 'restaurant_id',
@@ -27,5 +28,15 @@ db.menus.belongsTo(db.restaurants, {
     foreignKey: 'restaurant_id',
     sourceKey: 'id'
 });
+
+db.menus.hasMany(db.menuitems, {
+    foreignKey: 'menu_id',
+    sourceKey: 'id'
+});
+
+db.menuitems.belongsTo(db.menus, {
+    foreignKey: 'menu_id',
+    sourceKey: 'id'
+})
 
 module.exports = db;
